cc3364a721de8e7a2107ac85d6df9aa73bc3d1ed
auniraden <radennn.auni@gmail.com>
2025-02-10 12:31:23 +0800

    modify api routes 🛣️

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,8 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmailVerificationController;
 
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
 
 Route::get('/email/verify', [EmailVerificationController::class, 'verify'])->name('verification.verify');
